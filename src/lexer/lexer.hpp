e0f3188af4de25b7bf7bbc230a9d3dd97cc5a6c9
evlc <dodgedodgedoggy@gmail.com>
2026-05-28 11:46:57 +0700

    make lexers private methods private

--- src/lexer/lexer.hpp
+++ src/lexer/lexer.hpp
@@ -11,15 +11,15 @@ class Lexer {
   std::string_view source_;
   std::size_t offset_;
 
-public:
-  Lexer(std::string_view src) : source_(src), offset_(0) {}
-
   std::optional<char> peek();
   std::optional<char> eat();
 
   void skip_whitespace();
   std::string_view read_sym();
   int read_num();
+
+public:
+  Lexer(std::string_view src) : source_(src), offset_(0) {}
 
   Token scan_token();
 };
