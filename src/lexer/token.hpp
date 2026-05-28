777a922a17d0beffe2bb40e49d2216111bb27b8d
evlc <dodgedodgedoggy@gmail.com>
2026-05-28 12:34:21 +0700

    make value public as well

--- src/lexer/token.hpp
+++ src/lexer/token.hpp
@@ -13,15 +13,16 @@ class Token {
                                   std::monostate // all others
                                   >;
 
-  value_type value_;
   // std::string_view lexeme_;
   // int line_;                 // all three are TODO
   // int column_;
 
 public:
-  Token(TokenType type, value_type value) : value_(value), type(type) {}
+  Token(TokenType type, value_type value) : value(value), type(type) {}
 
   TokenType type;
+  value_type value;
+
   friend std::ostream &operator<<(std::ostream &os, const Token &t);
   bool is_eof() const;
 };
